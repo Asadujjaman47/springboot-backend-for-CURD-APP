688ac603a11f5dd51592472ab3824e043faa6d93
Asadujjaman <41225800+Asadujjaman47@users.noreply.github.com>
2023-04-30 12:42:26 +0600

    Creating JPA Entity and JPA Repository

---
+++ src/main/java/net/asad/springbootbackend/reposority/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package net.asad.springbootbackend.reposority;
+
+import net.asad.springbootbackend.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
